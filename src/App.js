e8f421c4b5e4a20e13fe1f7b1342a1ad7992880d
Ramon Mesquita <ramonmesquita113@gmail.com>
2019-09-11 18:29:32 -0300

    Convertendo functional component para class component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Tabela from './Tabela'
 
-function App () {
-  const books = [
-    {
-      name: 'React',
-      author: 'Ramon',
-      price: '199'
-    },
-    {
-      name: 'React Native',
-      author: 'Aline',
-      price: '599'
-    },
-    {
-      name: 'A vida secreta do Bernard',
-      author: 'Bernard',
-      price: '1099'
-    }
-  ];
+class App extends Component {
+  state = {
+    books: [
+      {
+        name: 'React',
+        author: 'Ramon',
+        price: '199'
+      },
+      {
+        name: 'React Native',
+        author: 'Aline',
+        price: '599'
+      },
+      {
+        name: 'A vida secreta do Bernard',
+        author: 'Bernard',
+        price: '1099'
+      }
+    ],
+  };
 
-  return (
-    <div className="App">
-      <Tabela books={books} />
-    </div>
-  );
+  render () {
+    return (
+      <div className="App">
+        <Tabela books={this.state.books} />
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
